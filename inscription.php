4e4762b8f8c976263ae37b6b50cafa3f9c518a2e
hafida88 <hafida.hazard@gmail.com>
2025-10-05 12:20:36 +0200

    structure de dossier incomplet

---
+++ inscription.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <h1>Inscription</h1>
+    <form>
+        <label for="nom">votre nom</label>
+        <input type="text" placeholder="Entrez votre nom" name="nom" id="nom">
+        <br>
+        <label for="prenom">votre prénom</label>
+        <input type="text" placeholder="Entrez votre prenom" name="prenom" id="prenom">
+    </form>
+</body>
+</html>
